54ae340457cc7ef15da3c8cde36ee26dd2266ac6
OuTiS92 <amirhoseinalahdadi76@gmail.com>
2026-09-09 18:38:04 +0330

    up: eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    webextensions: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
